87e6a4a079c021e8dcb6de6d233b90056504d704
Braden Hitchcock <developer.hitchcock@gmail.com>
2018-06-25 16:33:17 -0700

    initializing project library

---
+++ projects/logger/src/lib/logger.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngux-logger',
+  template: `
+    <p>
+      logger works!
+    </p>
+  `,
+  styles: []
+})
+export class LoggerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/logger/src/lib/logger.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { LoggerComponent } from './logger.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [LoggerComponent],
+  exports: [LoggerComponent]
+})
+export class LoggerModule { }
